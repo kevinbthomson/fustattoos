4490922889d527a63eb942f6a4c76e13e82c7f57
Kevin Thomson <kevinbthomson@gmail.com>
2023-06-22 17:45:26 -0600

    pulling WP contact data

--- src/_data/artists.js
+++ src/_data/artists.js
@@ -3,14 +3,14 @@ const EleventyFetch = require('@11ty/eleventy-fetch');
 module.exports = async function () {
   try {
     const response = await EleventyFetch(
-      `${process.env.WORDPRESS_REST_API_URL}artist`,
+      `${process.env.WORDPRESS_REST_API_URL}artist/?acf_format=standard`,
       {
         // duration: '1d',
         duration: '1s',
         type: 'json',
       }
     );
-    console.log(response);
+
     return response;
   } catch (error) {
     console.error('Eleventy Fetch error: ' + error);
--- src/_data/fus.js
+++ src/_data/fus.js
@@ -1,25 +1,47 @@
-module.exports = function () {
-  return {
-    name: "Fu's Custom Tattoos",
-    contact: {
-      phone: '(704) 376-4556',
-      email: 'fuscustomtattoos@gmail.com',
-      instagram: 'https://instagram.com/fuscustomtattoo',
-      address: '3200 N Davidson St. Charlotte, North Carolina',
-      hours: [
-        {
-          days: 'Monday - Thursday',
-          times: '12pm - 8pm',
-        },
-        {
-          days: 'Friday - Saturday',
-          times: '12pm - 8pm',
-        },
-        {
-          days: 'Sunday',
-          times: '12pm - 6pm',
-        },
-      ],
-    },
-  };
+// module.exports = function () {
+//   return {
+//     name: "Fu's Custom Tattoos",
+//     contact: {
+//       phone: '(704) 376-4556',
+//       email: 'fuscustomtattoos@gmail.com',
+//       instagram: 'https://instagram.com/fuscustomtattoo',
+//       address: '3200 N Davidson St. Charlotte, North Carolina',
+//       hours: [
+//         {
+//           days: 'Monday - Thursday',
+//           times: '12pm - 8pm',
+//         },
+//         {
+//           days: 'Friday - Saturday',
+//           times: '12pm - 8pm',
+//         },
+//         {
+//           days: 'Sunday',
+//           times: '12pm - 6pm',
+//         },
+//       ],
+//     },
+//   };
+// };
+
+const EleventyFetch = require('@11ty/eleventy-fetch');
+
+module.exports = async function () {
+  try {
+    const response = await EleventyFetch(
+      `${process.env.WORDPRESS_REST_API_URL}pages/?acf_format=standard`,
+      {
+        // duration: '1d',
+        duration: '1s',
+        type: 'json',
+      }
+    );
+
+    const options = response.filter((page) => page.slug === 'options');
+
+    const { acf } = options[0];
+    return acf;
+  } catch (error) {
+    console.error('Eleventy Fetch error: ' + error);
+  }
 };
