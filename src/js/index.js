1a7d37a9b3995fdbea6097ffe2f7428fa0a16dae
Kevin Thomson <kevinbthomson@gmail.com>
2023-11-30 14:47:46 -0700

    updated menu toggle graphic

--- src/js/index.js
+++ src/js/index.js
@@ -19,20 +19,23 @@ menuBtn.addEventListener('click', () => {
 const portfolioImageLinks = document.querySelectorAll('.portfolio a');
 let currentImage = 0;
 
-portfolioImageLinks.forEach((link, index) =>
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    imageModal(link, modal);
-    currentImage = index;
-    console.log(currentImage);
-  })
-);
+if (portfolioImageLinks && modal) {
+  portfolioImageLinks.forEach((link, index) =>
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      imageModal(link, modal);
+      currentImage = index;
+    })
+  );
+}
 
-modal.addEventListener('click', () => {
-  if (modal.dataset.open === 'true') {
-    modal.dataset.open = 'false';
-  }
-});
+if (modal) {
+  modal.addEventListener('click', () => {
+    if (modal.dataset.open === 'true') {
+      modal.dataset.open = 'false';
+    }
+  });
+}
 
 document.addEventListener('keyup', (e) => {
   if (body.dataset.mobileNavOpen === 'true' && e.key === 'Escape') {
@@ -44,8 +47,8 @@ document.addEventListener('keyup', (e) => {
   }
 
   if (modal.dataset.open === 'true' && e.key === 'ArrowRight') {
-    console.log(portfolioImageLinks.length);
-    if (currentImage === portfolioImageLinks.length) return;
+    console.log(currentImage, portfolioImageLinks.length);
+    if (currentImage + 1 === portfolioImageLinks.length) return;
 
     currentImage = currentImage += 1;
     imageModal(portfolioImageLinks[currentImage], modal);
